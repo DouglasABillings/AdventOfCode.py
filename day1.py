0509569a9d1c734d8f12ee791147fdfa66f0f22f
DouglasABillings <douglasbillings91@gmail.com>
2019-08-08 20:25:32 -0500

    Day 1 Part 2, not quite right

--- day1.py
+++ day1.py
@@ -1,13 +1,27 @@
+def duplicate(arr):
+    result = arr
+    duplicates = []
+    for i in range(len(result)):
+        k = i + 1
+        for j in range(k, len(result)):
+            if result[i] == result[j]:
+                duplicates.append(result[i])
+    print(duplicates)
+
+
 def main():
     file = open("day_1_input.txt", "r+")
     val = []
     for string in file:
         val.append(int(string))
     total = 0
+    result = []
     for i in range(len(val)):
-        total += val[i]
 
-    print(total)
+        total += val[i]
+        result.append(total)
+    print(result)
+    print(duplicate(result))
 
 
 if __name__ == "__main__":
