07db009ce4ff988ad8b1849d652b797dc3d36417
DouglasABillings <douglasbillings91@gmail.com>
2019-08-09 23:04:42 -0500

    Day 1 Finished

--- day1.py
+++ day1.py
@@ -1,27 +1,32 @@
-def duplicate(arr):
-    result = arr
-    duplicates = []
-    for i in range(len(result)):
-        k = i + 1
-        for j in range(k, len(result)):
-            if result[i] == result[j]:
-                duplicates.append(result[i])
-    print(duplicates)
+def frequency(input):
+    i = 0
+    current = 0
+    freq_list = {0}
+    while True:
+        current += input[i]
+        if current in freq_list:
+            return current
+        freq_list.add(current)
+        i += 1
+        if i == len(input):
+            i = 0
 
 
 def main():
     file = open("day_1_input.txt", "r+")
+
     val = []
     for string in file:
         val.append(int(string))
+
     total = 0
     result = []
     for i in range(len(val)):
-
         total += val[i]
         result.append(total)
-    print(result)
-    print(duplicate(result))
+
+    print(total)
+    print(frequency(val))
 
 
 if __name__ == "__main__":
